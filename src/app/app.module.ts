b0020cec976e74b66a08ef19df28b30ffa2e615f
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-06-08 11:10:26 -0400

    Add scream

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ScreamComponent } from './scream/scream.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, ScreamComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
