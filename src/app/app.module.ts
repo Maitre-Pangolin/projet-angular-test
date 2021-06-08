25d144d9d730ac06e1b5036fdd71791f82b35373
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-06-08 12:39:13 -0400

    change from mart local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ScreamComponent } from './scream/scream.component';
+import { MartlocalComponent } from './martlocal/martlocal.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ScreamComponent],
+  declarations: [AppComponent, HelloComponent, ScreamComponent, MartlocalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/martlocal/martlocal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-martlocal',
+  templateUrl: './martlocal.component.html',
+  styleUrls: ['./martlocal.component.css']
+})
+export class MartlocalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
