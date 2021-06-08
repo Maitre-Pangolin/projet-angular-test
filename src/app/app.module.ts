2ed67da77d18def2aae23cea32e776d0d788fcd7
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-06-08 10:52:05 -0400

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, TestComponent],
+  declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
